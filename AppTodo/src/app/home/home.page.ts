2f0646c602e4c77bf61b33627e3488dfe1eea528
Douglas Ribeiro <douglas_jipa@outlook.com>
2022-04-30 22:25:56 -0300

    Adicionando funcionalidades da pagina

--- AppTodo/src/app/home/home.page.ts
+++ AppTodo/src/app/home/home.page.ts
@@ -10,4 +10,37 @@ export class HomePage {
 
   constructor(private alertCrtl: AlertController) {}
 
+
+
+async showAdd() {
+  const alert = await this.alertCrtl.creat({
+    cssClass: 'my-custom-class',
+    header: 'O que você deseja fazer?',
+    inputs: [
+      {
+        name: 'tarefa1',
+        type: 'text',
+        placeholder: 'Digite o que deseja fazer.',
+      },
+    ],
+    buttons: [
+      {
+        text: 'Cancelar',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: () => {
+          console.log('Cancelado com sucesso!')
+        },
+      },
+      {
+        text: 'Adicionar',
+        handler: () => {
+          console.log('Adicionado com sucesso!')
+        },
+      },
+    ],
+  });
+
+  await alert.present();
+}
 }

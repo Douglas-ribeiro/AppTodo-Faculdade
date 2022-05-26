de66b4808f325e61d4878efcd42dd99a10193306
Douglas Ribeiro <douglas_jipa@outlook.com>
2022-05-25 22:12:10 -0300

    add suporte HTTP no projeto

--- AppTodo/src/app/home/home.module.ts
+++ AppTodo/src/app/home/home.module.ts
@@ -1,19 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { AppComponent } from '../app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from '../app-routing.module';
+import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
 
+
+//@NgModule({
+  //imports: [
+    //CommonModule,
+    //FormsModule,
+    //IonicModule,
+    //HomePageRoutingModule
+  //],
+  //declarations: [HomePage]
+//})
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
 })
 export class HomePageModule {}
